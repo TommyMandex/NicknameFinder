39a795ad2ec02e122921adc69168176a47835315
biu76 <biu76@tutanota.com>
2020-08-31 12:09:41 +0300

    added some new services

---
+++ services/pypi.py
@@ -0,0 +1,12 @@
+import requests
+import sys
+
+nickname = sys.argv[1]
+
+url = "https://pypi.org/user/" + nickname
+
+r = requests.get(url)
+if r.status_code == 200:
+    print(url)
+else:
+    sys.exit(1)
