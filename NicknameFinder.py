19c5c0759a073c1f7ccb23db9b9a4881aae7614f
biu76 <biu76@tutanota.com>
2020-08-31 10:38:54 +0300

    fix usage with argv

--- NicknameFinder.py
+++ NicknameFinder.py
@@ -1,15 +1,15 @@
 import os
-#import requests
+import sys
 import subprocess
 
-nickname = input("enter nickname: ")
+nickname = sys.argv[1]
 
-listdir = os.listdir("services/")
+listdir = os.listdir("services/") #ls dir with scripts
 
 for filename in listdir:
     try:
         output = subprocess.check_output("python3 services/{0} {1}".format(filename,nickname), universal_newlines=True, shell=True)
-        output = output[0:len(output)-1]
+        output = output[0:len(output)-1] #remove \n
         print("[{0}] found: {1}".format(filename[0:len(filename)-3], output))
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError: #if exit-code non-zero
         print("[{0}] not found".format(filename[0:len(filename)-3]))
